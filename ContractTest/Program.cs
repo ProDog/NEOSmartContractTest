d6ab0644a557b53805c4925dc130f7e19f8f4e79
ProDog <xiuboard@foxmail.com>
2018-08-28 19:42:22 +0800

    Hello world

---
+++ ContractTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Neo.VM;
+
+namespace ContractTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var noparamAVM = System.IO.File.ReadAllBytes("E:\\VSProject\\NeoContractTest\\NeoContracts\\bin\\Debug\\NeoContracts.avm");
+            var str = Neo.Helper.ToHexString(noparamAVM);
+
+            Neo.VM.ScriptBuilder sb = new ScriptBuilder();
+            sb.EmitPush(12);
+            sb.EmitPush(14);
+            sb.EmitPush(2);
+            sb.Emit(Neo.VM.OpCode.PACK);
+            sb.EmitPush("parame");
+
+            var _parames = sb.ToArray();
+            var str2 = Neo.Helper.ToHexString(_parames);
+
+
+            Console.WriteLine("AVM=" + str2 + str);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NeoContracts/Contract1.cs
@@ -0,0 +1,23 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.Numerics;
+
+namespace NeoContracts
+{
+    public class Contract1 : SmartContract
+    {
+        public static void Main()
+        {
+            Storage.Put(Storage.CurrentContext, "Hello", "World");
+            //var test = "hello world!";
+            //return test;
+        }
+
+        //public static object Main(string param, int[] value)
+        //{
+        //    var magicstr = "2018 02 21";
+        //    return value[0] + value[1];
+        //}
+    }
+}
